21b34aa719c2a763d933879e1ad2d00c8601bdd3
dalleth-martinss <essi_dalleth@outlook.com>
2024-06-27 22:25:14 -0300

    Creating methods

---
+++ Entities/Post.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ExerciceStringBuilderPostSocialMedia.Entities
+{
+    internal class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public  int Likes { get; set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>(); // garantir que vai iniciar uma lista vazia do tipo Comment
+    
+        public Post()
+        {
+
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            
+        }
+
+
+    }
+} 
+
+//STRING BUILDER => é responsavel pela montagem do formato do output em string conforme eu quero
+// de uma forma super eficiente, depois que tiver pronto pego o resuldo disso e passo para strig
+//AppendLine() => adiciona texto no fim do string builder e coloca quebra de linha 
+
+
+
+// Quando temos um diagrama com " relação tem muitos"  ---> * (seta com asterisco)
+// fazemos os metodos Adicionar e remover. 
--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using ExerciceStringBuilderPostSocialMedia.Entities;
 
 namespace ExerciceStringBuilderPostSocialMedia
 {
@@ -10,6 +9,30 @@ namespace ExerciceStringBuilderPostSocialMedia
     {
         static void Main(string[] args)
         {
+            Comment c1 = new Comment("Have a nice trip ");
+            Comment c2 = new Comment("Wow that's awesome!");
+
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018 22:08:58"),
+                "Traveling to new Zealand",
+                "I'm going to visit this wonderful country! ",
+                12
+                );
+
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good evening!");
+            Comment c4 = new Comment("May the Force be with you ");
+            Post p2 = new Post(
+                DateTime.Parse("28/07/2024  22:13:51"),
+                "I'll to sleep now",
+                "See you tomorrow",
+                5);
+
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
         }
     }
 }
